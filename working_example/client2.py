b65e0ee28c68beb1d60b769b0e8939c43f36dbb4
nicholas_lambiase <nicholaslambiase14@gmail.com>
2024-02-13 15:26:07 -0600

    Playing around with IP and Port numbers for UDP Connections

--- working_example/client2.py
+++ working_example/client2.py
@@ -4,10 +4,13 @@ import socket
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # binding the socket to the host IP and Port to listen and send on
-clientSocket.bind(('127.0.0.2',12346))
+clientSocket.bind(('127.0.100.2',12567))
 
 messageToManager = ("Hello UDP Server... I am a peer that just contacted you!").encode('utf-8')
-clientSocket.sendto(messageToManager, ('127.0.0.1', 12345))
+clientSocket.sendto(messageToManager, ('127.0.100.1', 12345))
 data, addr = clientSocket.recvfrom(4096)
-print("Server says\n" + data.decode())
+print("Server says\n" + data.decode() + "\n")
+print("Manager Response from:\n" +
+      "IPV4 Address = " + addr[0] +
+      "\nPort Number = " + str(addr[1]) + "\n\n")
 clientSocket.close()
--- working_example/manager2.py
+++ working_example/manager2.py
@@ -4,12 +4,16 @@ import socket
 managerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # binding the socket to the host IP and Port to listen and send on
-managerSocket.bind(('127.0.0.1',12345))
+managerSocket.bind(('10.0.2.15',12345))
 
 print("Manager up and listening")
 
 while True:
     mssg, addr = managerSocket.recvfrom(4096)
-    print(mssg.decode())
-    response = ("Reached the Manager Server").encode('utf-8')
+    print(mssg.decode() + "\n")
+    print("Client Message from:\n" +
+      "IPV4 Address = " + addr[0] +
+      "\nPort Number = " + str(addr[1]) + "\n\n")
+    
+    response = ("This is the Response from the Manager Server").encode('utf-8')
     managerSocket.sendto(response, addr)
