5e185051f9155aaaf106e7096bc251deb5b212c4
nicholas_lambiase <nicholaslambiase14@gmail.com>
2024-02-13 21:15:22 -0600

    Updated the template scripts

--- working_example/client2.py
+++ working_example/client2.py
@@ -4,10 +4,10 @@ import socket
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # binding the socket to the host IP and Port to listen and send on
-clientSocket.bind(('127.0.100.2',12567))
+clientSocket.bind(('10.0.2.15',9090))
 
-messageToManager = ("Hello UDP Server... I am a peer that just contacted you!").encode('utf-8')
-clientSocket.sendto(messageToManager, ('127.0.100.1', 12345))
+messageToManager = ("Hello UDP Server... ").encode('utf-8')
+clientSocket.sendto(messageToManager, ('100.64.5.122', 12345))
 data, addr = clientSocket.recvfrom(4096)
 print("Server says\n" + data.decode() + "\n")
 print("Manager Response from:\n" +
