467b9156a2afa1277469d34792424ecd0f6f0a7d
Ahmahsamara17 <ahmadsamara177@gmail.com>
2024-02-16 11:41:40 -0700

    skeleton of the hashmaping
    logic for the ring command

--- client.py
+++ client.py
@@ -15,13 +15,34 @@ ENCODER = "utf-8"
 
 SETUPBOOL = False
 identifier = 0
+addr_of_right_neighbor = 0
+right_neighbor = ("", 0)
 MAX_UDP_SIZE = 65507
 size_of_ring = 0
+
+local_hash_table = {}
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client_socket.bind((SOURCE_IP, SOURCE_PORT))
 
 print(SOURCE_PORT)
+
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def next_prime_after_2l(file_size):
+    num = 2 * file_size + 1
+    while True:
+        if is_prime(num):
+            return num
+        num += 2
 
 
 def split_the_message(full_message):
@@ -30,7 +51,7 @@ def split_the_message(full_message):
 
 
 def set_id(peer_tuple_data, id_number, size, full_tuple_data):
-    #print((peer_tuple_data[1], peer_tuple_data[2]))
+    # print((peer_tuple_data[1], peer_tuple_data[2]))
 
     obj_to_send = "set-id", id_number, size, full_tuple_data
     serialized_obj = pickle.dumps(obj_to_send)
@@ -48,23 +69,45 @@ def receive():
 
             global size_of_ring
             global identifier
-            #print(SETUPBOOL)
+            global addr_of_right_neighbor
+            global right_neighbor
+
+            # event_id = current_entry[0]
+            event_id = 0
+            big_prime = next_prime_after_2l(event_id)
 
             if SETUPBOOL:
                 code, list_of_dht, year = data
 
-                size_of_ring = len(list_of_dht)
-
                 identifier = 0
+                size_of_ring = len(list_of_dht)
+                addr_of_right_neighbor = (identifier + 1) % size_of_ring
+                right_neighbor = list_of_dht[addr_of_right_neighbor]
 
                 for i in range(1, size_of_ring):
-                    #print(i)
+                    # print(i)
                     set_id(list_of_dht[i], i, size_of_ring, list_of_dht)
 
-            command1, id_data, size, neighbor = data
-            #print(command1)
+                pos = event_id % big_prime
+                node_id = pos % size_of_ring
 
-            if command1 == "set-id":
+                # a block here to check if the leader should store the record
+                #
+                # if node_id == identifier:
+                #       local_hash_table[pos].append(current_entry)
+                #
+                #######
+                #
+                ## sending the record to the right neighbor
+
+                packet = "store", node_id, pos  # , record
+                serialized_packet = pickle.dumps(packet)
+                client_socket.sendto(serialized_packet, (right_neighbor[1], int(right_neighbor[2])))
+
+            # print(command1)
+
+            if data[0] == "set-id" and not SETUPBOOL:
+                command1, id_data, size, neighbor = data
                 identifier = id_data
                 size_of_ring = size
                 addr_of_right_neighbor = (identifier + 1) % size_of_ring
@@ -73,6 +116,20 @@ def receive():
                 # print(size_of_ring)
                 # print(addr_of_right_neighbor)
                 # print(right_neighbor)
+
+            if data[0] == "store" and not SETUPBOOL:
+                # a block here to check if the peer should store the record
+                command2, node_id, node_position, entry = data
+                # event_id = entry[0]
+                prime = next_prime_after_2l(event_id)
+                my_pos = event_id % prime
+                my_id = my_pos % size_of_ring
+                if node_id == my_id:
+                    local_hash_table[my_pos].append(entry)
+                else:
+                    packet = "store", my_id, node_position, entry
+                    serialized_packet = pickle.dumps(packet)
+                    client_socket.sendto(serialized_packet, (right_neighbor[1], int(right_neighbor[2])))
             else:
                 # print(data)
                 pass
